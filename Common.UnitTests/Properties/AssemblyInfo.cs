63a675af7c7281ca2a574e8be43713dd7fd5e60b
Konstantin Burov <burov.konstantin@gmail.com>
2016-02-09 15:08:51 +0200

    Add empty Common.UnitTests project

---
+++ Common.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Helpers.Common.UnitTests")]
+[assembly: AssemblyDescription("Unit tests for common helper classes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wanderer")]
+[assembly: AssemblyProduct("Helpers.Common.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Wanderer 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6b8d2750-e347-423e-8cc7-4fc13901cac3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
