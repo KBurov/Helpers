cbe47d16cbbf274ba1ee472710079ec69aeb5a68
Konstantin Burov <burov.konstantin@gmail.com>
2016-02-09 15:30:40 +0200

    Add ContextBase - base class for any types of tests

---
+++ TestFramework/ContextBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Helpers.TestFramework
+{
+    /// <summary>
+    /// Base class for either unit or integration tests context classes.
+    /// </summary>
+    public abstract class ContextBase : IDisposable
+    {
+        private bool _disposed;
+
+        #region IDisposable implementation
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resource.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            Contract.Ensures(_disposed);
+
+            if (!_disposed) {
+                if (disposing) {
+                    Cleanup();
+                }
+
+                _disposed = true;
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Parameterless constructor to support test setup logic.
+        /// </summary>
+        protected ContextBase()
+        {
+            _disposed = false;
+
+            SetUp();
+        }
+
+        /// <summary>
+        /// Finalizer.
+        /// </summary>
+        ~ContextBase()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Implement in derived classes for test setup logic.
+        /// </summary>
+        protected virtual void SetUp() {}
+
+        /// <summary>
+        /// Implement in derived classes for test cleanup logic.
+        /// </summary>
+        protected virtual void Cleanup() {}
+    }
+}
