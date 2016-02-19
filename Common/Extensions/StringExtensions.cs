12796850c82374069bfc94cb6fef76d3897b2aa7
Konstantin Burov <burov.konstantin@gmail.com>
2016-02-19 17:21:16 +0200

    Extend StringExtensions class with the new method - Reverse

--- Common/Extensions/StringExtensions.cs
+++ Common/Extensions/StringExtensions.cs
@@ -11,8 +11,8 @@ namespace Helpers.Common.Extensions
         /// <summary>
         /// Converts the specified string to string with the first capital letter.
         /// </summary>
-        /// <param name="s"></param>
-        /// <returns></returns>
+        /// <param name="s">the string to convert</param>
+        /// <returns>the specified string converted to string with the first capital letter</returns>
         public static string FirstCharToUpper(this string s)
         {
             Contract.Requires<ArgumentNullException>(s != null, "s cannot be null");
@@ -23,6 +23,27 @@ namespace Helpers.Common.Extensions
             }
 
             return s.ToUpper();
+        }
+
+        /// <summary>
+        /// Converts the specified string to string with reverted value.
+        /// </summary>
+        /// <param name="s">the string to convert</param>
+        /// <returns>the specified string converted to string with reverted value</returns>
+        public static string Reverse(this string s)
+        {
+            // TODO: Look at http://stackoverflow.com/questions/228038/best-way-to-reverse-a-string
+            Contract.Requires<ArgumentNullException>(s != null, "s cannot be null");
+            Contract.Ensures(Contract.Result<string>() != null);
+
+            if (s == string.Empty) {
+                return string.Empty;
+            }
+
+            var charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+
+            return new string(charArray);
         }
 
         #region Is... methods
