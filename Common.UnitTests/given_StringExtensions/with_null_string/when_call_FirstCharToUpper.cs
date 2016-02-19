d948042e5f17385f74ecab6248424510c3c0e1fb
Konstantin Burov <konstantin.burov@hotmail.com>
2016-02-19 22:37:38 +0200

    Add unit tests for StringExtensions class for case with null string value

---
+++ Common.UnitTests/given_StringExtensions/with_null_string/when_call_FirstCharToUpper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Helpers.Common.Extensions;
+
+using Xunit;
+
+namespace Helpers.Common.UnitTests.given_StringExtensions.with_null_string
+{
+    public class when_call_FirstCharToUpper : Context
+    {
+        [Fact]
+        public void then_then_throws_exception()
+        {
+            Assert.Throws<ArgumentNullException>(() => _str.FirstCharToUpper());
+        }
+    }
+}
