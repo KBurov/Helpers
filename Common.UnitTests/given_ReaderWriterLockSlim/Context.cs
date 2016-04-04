7a80c2952f8d19ced8906fdfaf98601a18098a3e
Konstantin Burov <burov.konstantin@gmail.com>
2016-04-04 17:15:17 +0300

    Add unit tests for extension methods
    Add only some of the unit tests for the ReaderWriterLockSlim extension
    methods

---
+++ Common.UnitTests/given_ReaderWriterLockSlim/Context.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+
+using Helpers.TestFramework;
+
+namespace Helpers.Common.UnitTests.given_ReaderWriterLockSlim
+{
+    public abstract class Context : ContextBase
+    {
+        protected ReaderWriterLockSlim _locker = null;
+    }
+}
---
+++ Common.UnitTests/given_ReaderWriterLockSlim/with_null_locker/when_call_GetReadLock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Helpers.Common.Extensions.Threading;
+
+using Xunit;
+
+namespace Helpers.Common.UnitTests.given_ReaderWriterLockSlim.with_null_locker
+{
+    public sealed class when_call_GetReadLock : Context
+    {
+        [Fact]
+        public void then_throws_exception()
+        {
+            Assert.Throws<ArgumentNullException>(() => _locker.GetReadLock());
+        }
+    }
+}
