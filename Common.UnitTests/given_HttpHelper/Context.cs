589f16eb7922d7f95cc721533fecee2891c3f03f
Konstantin Burov <burov.konstantin@gmail.com>
2016-07-26 15:28:17 +0300

    Add base class for unit tests of HttHelper

---
+++ Common.UnitTests/given_HttpHelper/Context.cs
@@ -0,0 +1,21 @@
+﻿using Helpers.TestFramework;
+
+namespace Helpers.Common.UnitTests.given_HttpHelper
+{
+    public abstract class Context : ContextBase
+    {
+        protected IHttpHelper _httpHelper;
+
+        protected Context()
+        {
+            SetUp();
+        }
+
+        protected sealed override void SetUp()
+        {
+            base.SetUp();
+
+            _httpHelper = new HttpHelper();
+        }
+    }
+}
