39cba23e7c956857857cf32f2233d9e617276ea9
Konstiantyn Burov <konstantin.burov@hotmail.com>
2017-11-01 02:25:01 -0400

    Add async versions of existing methods

--- Common/Retry.cs
+++ Common/Retry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Helpers.Common
 {
@@ -9,7 +10,7 @@ namespace Helpers.Common
     public static class Retry
     {
         /// <summary>
-        /// Repeat action <paramref name="numberOfRetries"/> times with <paramref name="timeoutInMilliseconds"/> delay.
+        /// Repeat action <paramref name="numberOfRetries" /> times with <paramref name="timeoutInMilliseconds" /> delay.
         /// </summary>
         /// <param name="action">repeatable action</param>
         /// <param name="numberOfRetries">number of times to repeat</param>
@@ -22,7 +23,7 @@ namespace Helpers.Common
             if (numberOfRetries > 0u) {
                 var repeat = true;
 
-                while (repeat) {
+                while (repeat)
                     try {
                         action();
 
@@ -35,12 +36,41 @@ namespace Helpers.Common
                         if (timeoutInMilliseconds.HasValue)
                             Thread.Sleep((int) timeoutInMilliseconds.Value);
                     }
-                }
             }
         }
 
         /// <summary>
-        /// Repeat func <paramref name="numberOfRetries"/> times with <paramref name="timeoutInMilliseconds"/> delay.
+        /// Repeat action <paramref name="numberOfRetries" /> times with <paramref name="timeoutInMilliseconds" /> delay (async version).
+        /// </summary>
+        /// <param name="action">repeatable action</param>
+        /// <param name="numberOfRetries">number of times to repeat</param>
+        /// <param name="timeoutInMilliseconds">delay between repeating action</param>
+        public static async void TimesAsync(Func<Task> action, uint numberOfRetries, uint? timeoutInMilliseconds = null)
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action), $"{nameof(action)} cannot be null");
+
+            if (numberOfRetries > 0u) {
+                var repeat = true;
+
+                while (repeat)
+                    try {
+                        await action();
+
+                        repeat = false;
+                    }
+                    catch {
+                        if (--numberOfRetries == 0u)
+                            throw;
+
+                        if (timeoutInMilliseconds.HasValue)
+                            await Task.Delay((int) timeoutInMilliseconds.Value);
+                    }
+            }
+        }
+
+        /// <summary>
+        /// Repeat func <paramref name="numberOfRetries" /> times with <paramref name="timeoutInMilliseconds" /> delay.
         /// </summary>
         /// <typeparam name="T">type of func result</typeparam>
         /// <param name="func">repeatable func</param>
@@ -54,18 +84,45 @@ namespace Helpers.Common
             if (numberOfRetries == 0)
                 throw new ArgumentException($"{nameof(numberOfRetries)} should be greater than 0");
 
-            while (true) {
+            while (true)
                 try {
                     return func();
                 }
                 catch {
                     if (--numberOfRetries == 0u)
                         throw;
-                    // TODO: Replace Thread.Sleep with more modern implementation
+
                     if (timeoutInMilliseconds.HasValue)
                         Thread.Sleep((int) timeoutInMilliseconds.Value);
                 }
-            }
+        }
+
+        /// <summary>
+        /// Repeat func <paramref name="numberOfRetries" /> times with <paramref name="timeoutInMilliseconds" /> delay (async version).
+        /// </summary>
+        /// <typeparam name="T">type of func result</typeparam>
+        /// <param name="func">repeatable func</param>
+        /// <param name="numberOfRetries">number of times to repeat</param>
+        /// <param name="timeoutInMilliseconds">delay between repeating action</param>
+        /// <returns>func result</returns>
+        public static async Task<T> TimesAsync<T>(Func<Task<T>> func, uint numberOfRetries, uint? timeoutInMilliseconds = null)
+        {
+            if (func == null)
+                throw new ArgumentNullException(nameof(func), $"{nameof(func)} cannot be null");
+            if (numberOfRetries == 0)
+                throw new ArgumentException($"{nameof(numberOfRetries)} should be greater than 0");
+
+            while (true)
+                try {
+                    return await func();
+                }
+                catch {
+                    if (--numberOfRetries == 0u)
+                        throw;
+
+                    if (timeoutInMilliseconds.HasValue)
+                        await Task.Delay((int) timeoutInMilliseconds.Value);
+                }
         }
     }
 }
