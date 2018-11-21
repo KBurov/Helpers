b2f42abcec933aab109c22e0269a7cb87201cfc1
Konstiantyn Burov <konstantin.burov@hotmail.com>
2018-11-21 00:20:36 -0500

    Add utility classes - MathFuncs and DummyStopTermHandler

---
+++ StringSim/Utils/MathFuncs.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Helpers.StringSim.Utils
+{
+    /// <summary>
+    /// Number of handy maths functions.
+    /// </summary>
+    public static class MathFuncs
+    {
+        /// <summary>
+        /// Returns the max of three numbers.
+        /// </summary>
+        /// <param name="firstNumber">first number to test</param>
+        /// <param name="secondNumber">second number to test</param>
+        /// <param name="thirdNumber">third number to test</param>
+        /// <returns>the max of three numbers</returns>
+        public static double MaxOf3(double firstNumber, double secondNumber, double thirdNumber)
+        {
+            return Math.Max(firstNumber, Math.Max(secondNumber, thirdNumber));
+        }
+
+        /// <summary>
+        /// Returns the max of three numbers.
+        /// </summary>
+        /// <param name="firstNumber">first number to test</param>
+        /// <param name="secondNumber">second number to test</param>
+        /// <param name="thirdNumber">third number to test</param>
+        /// <returns>the max of three numbers</returns>
+        public static int MaxOf3(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return Math.Max(firstNumber, Math.Max(secondNumber, thirdNumber));
+        }
+
+        /// <summary>
+        /// Returns the max of four numbers.
+        /// </summary>
+        /// <param name="firstNumber">first number to test</param>
+        /// <param name="secondNumber">second number to test</param>
+        /// <param name="thirdNumber">third number to test</param>
+        /// <param name="fourthNumber">fourth number to test</param>
+        /// <returns>the max of four numbers</returns>
+        public static double MaxOf4(double firstNumber, double secondNumber, double thirdNumber, double fourthNumber)
+        {
+            return Math.Max(Math.Max(firstNumber, secondNumber), Math.Max(thirdNumber, fourthNumber));
+        }
+
+        /// <summary>
+        /// Returns the min of three numbers.
+        /// </summary>
+        /// <param name="firstNumber">first number to test</param>
+        /// <param name="secondNumber">second number to test</param>
+        /// <param name="thirdNumber">third number to test</param>
+        /// <returns>the min of three numbers</returns>
+        public static double MinOf3(double firstNumber, double secondNumber, double thirdNumber)
+        {
+            return Math.Min(firstNumber, Math.Min(secondNumber, thirdNumber));
+        }
+
+        /// <summary>
+        /// Returns the min of three numbers.
+        /// </summary>
+        /// <param name="firstNumber">first number to test</param>
+        /// <param name="secondNumber">second number to test</param>
+        /// <param name="thirdNumber">third number to test</param>
+        /// <returns>the min of three numbers</returns>
+        public static int MinOf3(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return Math.Min(firstNumber, Math.Min(secondNumber, thirdNumber));
+        }
+    }
+}
---
+++ StringSim/Utils/WordHandlers/DummyStopTermHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+using Helpers.StringSim.Base;
+
+namespace Helpers.StringSim.Utils.WordHandlers
+{
+    /// <summary>
+    /// Implements a dummy stop word handling function whereby no stopwords are considered.
+    /// </summary>
+    public sealed class DummyStopTermHandler : ITermHandler
+    {
+        #region ITermHandler implementation
+        /// <inheritdoc />
+        public void AddWord(string termToAdd) {}
+
+        /// <inheritdoc />
+        public bool IsWord(string termToTest) => false;
+
+        /// <inheritdoc />
+        public void RemoveWord(string termToRemove) {}
+
+        /// <inheritdoc />
+        public int NumberOfWords => 0;
+
+        /// <inheritdoc />
+        public string ShortDescriptionString => "DummyStopTermHandler";
+
+        /// <inheritdoc />
+        public StringBuilder WordsAsBuffer => new StringBuilder();
+        #endregion
+    }
+}
